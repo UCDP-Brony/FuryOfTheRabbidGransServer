d760b82822f3a2f0f8d8596b2705c6a29e7d5b80
TheDoctor <TheDoctor@TARDIS>
2016-07-12 14:38:56 +0200

    added password check.

--- src/thefuryoftherabbidgrans/server/RoomMatching.java
+++ src/thefuryoftherabbidgrans/server/RoomMatching.java
@@ -6,12 +6,24 @@
 package thefuryoftherabbidgrans.server;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 import thefuryoftherabbidgrans.server.implementations.Player;
 import thefuryoftherabbidgrans.server.interfaces.Player_INTERFACE;
 
@@ -24,7 +36,7 @@ class RoomMatching implements Runnable {
     private PrintWriter out;
     private BufferedReader in;
     private String room;
-    private boolean roomed;
+    private boolean roomed, validPlayer;
     private Thread t2;
     
     public RoomMatching(Socket socket) {
@@ -33,6 +45,7 @@ class RoomMatching implements Runnable {
         this.in = null;
         this.room = null;
         this.roomed = false;
+        this.validPlayer = false;
     }
 
     @Override
@@ -42,30 +55,13 @@ class RoomMatching implements Runnable {
             out = new PrintWriter(socket.getOutputStream());
             
             Player_INTERFACE p = new Player(in, out);
-            
-            sendMessage("C210");
-            String name = in.readLine();            
+            String name = validatePlayer();            
             p.initPlayer(name);
-            RoomManager rm = RoomManager.getInstance();
-            while (!roomed){
-                sendMessage("C212");
-                room = in.readLine();
-                if(rm.roomExists(room)){
-                    if(rm.canAddToRoom(room)){
-                        rm.addPlayerToRoom(room, p);
-                        roomed = true;
-                    } else {
-                        sendMessage("C413");
-                        roomed = false;
-                    }
-                }else{
-                    rm.addEntry(room);
-                    rm.addPlayerToRoom(room, p);
-                    roomed = true;
-                } 
-            }
-            System.out.println(p.getName()+" joined the room "+room+". There are "+rm.getRoomByID(room).getNbPlayers()+" players in the room.");
-        } catch (IOException ex) {
+            roomPlayer(p);                        
+            
+        } catch (IOException | SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchAlgorithmException ex) {
+            sendMessage("C500");
+        } catch (ParserConfigurationException | SAXException ex) {
             Logger.getLogger(RoomMatching.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -73,5 +69,70 @@ class RoomMatching implements Runnable {
     private void sendMessage(String s){
         out.println(s);
         out.flush();
+    }
+
+    private String bytesToHex(byte[] b) {
+        char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+      StringBuilder buf = new StringBuilder();
+      for (int j=0; j<b.length; j++) {
+         buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+         buf.append(hexDigit[b[j] & 0x0f]);
+      }
+      return buf.toString();
+    }
+
+    private String validatePlayer() throws ClassNotFoundException, IOException, InstantiationException, IllegalAccessException, SQLException, NoSuchAlgorithmException, ParserConfigurationException, SAXException {
+        String name = "";
+        String pass = "";
+        while(!validPlayer){
+            sendMessage("C210");
+            name = in.readLine();
+            sendMessage("C211");
+            pass = in.readLine();
+            File passFile = new File("./src/thefuryoftherabbidgrans/ressources/pass.xml");
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(passFile);
+            doc.getDocumentElement().normalize();
+                                    
+            String password = doc.getElementsByTagName("pass").item(0).getTextContent();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gransCommunity","joachim",password);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT mot_de_passe FROM gransCommunity.membres WHERE nom_utilisateur='"+name+"';");
+            if(rs.next()){
+                MessageDigest md = MessageDigest.getInstance("SHA1");
+                md.update(pass.getBytes()); 
+                byte[] output = md.digest();
+                if(rs.getString(1).equals(bytesToHex(output))){
+                    this.validPlayer = true;
+                    sendMessage("C211_1");
+                } else {
+                    sendMessage("C411");
+                }
+            }
+        }
+        return name;
+    }
+    
+    private void roomPlayer(Player_INTERFACE p) throws IOException{
+        RoomManager rm = RoomManager.getInstance();
+        while (!roomed){
+            sendMessage("C212");
+            room = in.readLine();
+            if(rm.roomExists(room)){
+                if(rm.canAddToRoom(room)){
+                    rm.addPlayerToRoom(room, p);
+                    roomed = true;
+                } else {
+                    sendMessage("C413");
+                    roomed = false;
+                }
+            }else{
+                rm.addEntry(room);
+                rm.addPlayerToRoom(room, p);
+                roomed = true;
+            } 
+        }
+        System.out.println(p.getName()+" joined the room "+room+". There are "+rm.getRoomByID(room).getNbPlayers()+" players in the room.");
     }
 }
